04c8929e0d2f57e6af567cde3f0fbc4d15a91d1b
iammagash <74823831+Magashwarahan@users.noreply.github.com>
2021-08-01 11:39:00 +0530

    Update [slug].js

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -64,7 +64,7 @@ export default function Post(props) {
 
       <aside className="card">
         <p>
-          <strong>{post.heartCount || 0} 🤍</strong>
+          <strong>{post.heartCount || 0}💗</strong>
         </p>
 
         <AuthCheck
